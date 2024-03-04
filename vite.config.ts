3bb7e6854019c19fca966d7b62479ca238f21991
Nghiem Le <nghiemle@Nghiems-MacBook-Pro.local>
2024-03-04 18:11:37 +0700

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: { chunkSizeWarningLimit: 2000, },
     /* Config Alias */
     resolve: {
       alias: {
